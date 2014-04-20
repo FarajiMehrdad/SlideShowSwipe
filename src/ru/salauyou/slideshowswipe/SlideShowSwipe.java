b85dff5c78c8a17ab5d501b4a18e1f1cc7e84198
Salauyou <denisie@ya.ru>
2014-04-20 05:27:36 +0400

    Implemented bitmap tracking; added callbacks to notify state changes
    Now the bitmap that is displayed by the view (still or moving due to
    user's swipe) corresponds to cursor position in bimap container--i. e.
    can be obtained by `getBitmapCurrent()`.

--- src/ru/salauyou/slideshowswipe/SlideShowSwipe.java
+++ src/ru/salauyou/slideshowswipe/SlideShowSwipe.java
@@ -13,22 +13,75 @@ import android.view.View;
 
 public class SlideShowSwipe extends View {
 
+	/**
+	 * Interface to control bitmap container
+	 */
 	public interface BitmapContainer {
 		
+		/**
+		 * Request of next bitmap. Container should shift its cursor to next bitmap and return it 
+		 */
 		public Bitmap getBitmapNext();
 		
+		/**
+		 * Request of previous bitmap. Container should shift its cursor to previous bitmap and return it
+		 */
 		public Bitmap getBitmapPrevious();
 		
+		/**
+		 * Request of current bitmap. Container shoud return bitmap at its current cursor position.
+		 * 
+		 * Container must return the same (or similar) bitmap as returned by preceding {@code getBitmapCurrent()},
+		 * {@code getBitmapNext()} or {@code getBitmapPrevious()} invocation.
+		 */
 		public Bitmap getBitmapCurrent();
 		
+		/**
+		 * Request to restore cursor position to the position before the latest request. 
+		 * 
+		 * Container should restore its cursor position to the position right before the latest request.
+		 * For example, if previously {@code getBitmapCurrent()} returned bitmap A, then {@code getBitmapNext()} 
+		 * and {@code undoGetBitmap()} were called, invocation of {@code getBitmapCurrent()} must return 
+		 * the same (or similar) bitmap A, independently of any changes that could happen to bitmap container 
+		 * between such calls. It is guaranteed that {@code undoGetBitmap()} will be invoked at most once after 
+		 * each {@code getBitmapNext} or {@code getBitmapPrevious()} request. 
+		 */
 		public void undoGetBitmap();
 		
+	}
+	
+	/**
+	 * Interface to send callbacks when state is changed
+	 */
+	public interface OnStateChangeListener {
+		
+		/**
+		 * Invoked when the view changed its status
+		 */
+		public void onStateChange(SlideShowSwipe.State state);
+		
+		/**
+		 * Invoked when current bitmap was changed and now is demonstrated during slide show 
+		 * or as the result of user interaction. After this callback, current displayed bitmap
+		 * can be obtained by {@code BitmapContainer.getBitmapCurrent()} method
+		 */
+		public void onCurrentBitmapChange();
+	}
+	
+	/**
+	 * Possible states of the view
+	 */
+	public enum State{
+		STOPPED, TOUCHED, RELEASED, NEXT_SLIDE; 
 	}
 	
 	final private SlideShowSwipe self = this;
 	
 	private BitmapContainer container;
 	private Bitmap bitmapFront, bitmapBack;
+	
+	private State stateCurrent = State.STOPPED;
+	private OnStateChangeListener stateChangeListener;
 	
 	private Rect rectDstBOrig, rectDstFOrig;
 	private Rect rectDstF = new Rect();
@@ -47,7 +100,55 @@ public class SlideShowSwipe extends View {
 	private float kVScreen = 4f;	// deceleration coefficient relative to view width
 	private float kV; // deceleration coefficient in px/s^2
 	
+	private boolean undoAtZero = false;
 	
+	
+
+	/* ===================== Publc methods ====================== */
+	
+	/**
+	 * Sets bitmap container to be controlled by this view
+	 * 
+	 * @throws NullPointerException	
+	 */
+	public SlideShowSwipe setBitmapContainer(BitmapContainer container) throws NullPointerException{
+		if (container == null)
+			throw new NullPointerException("Container is null");
+		this.container = container;
+		return this;
+	}
+	
+	/**
+	 * Sets listener of state changes
+	 * 
+	 * @throws NullPointerException
+	 */
+	public SlideShowSwipe setOnStateChangeListener(OnStateChangeListener listener) throws NullPointerException{
+		if (listener == null)
+			throw new NullPointerException("Listener is null");
+		this.stateChangeListener = listener;
+		return this;
+	}
+	
+	/**
+	 * Gets bitmap container controlled by this view
+	 * 
+	 * @return
+	 */
+	public SlideShowSwipe.BitmapContainer getBitmapContainer(){
+		return this.container;
+	}
+	
+	/**
+	 * Returns the current state of this view
+	 */
+	public State getState(){
+		return this.stateCurrent;
+	}
+	
+	
+	
+	/* ============== Constructors inherited from View ================ */
 	
 	public SlideShowSwipe(Context context) {
 		super(context);
@@ -63,20 +164,9 @@ public class SlideShowSwipe extends View {
 		super(context, attrs, defStyleAttr);
 		setGestureListener();
 	}
-
 	
-	/**
-	 * Sets bitmap container to be controlled by this view
-	 * 
-	 * @param 	container	bitmap contaner, must implement {@code BitmapContainer}
-	 * @return	
-	 */
-	public SlideShowSwipe setBitmapContainer(BitmapContainer container){
-		if (container != null)
-			this.container = container;
-		return this;
-	}
 	
+	/* ================ Protected and private methods ================== */
 	
 	/**
 	 * Set gesture listener
@@ -94,7 +184,8 @@ public class SlideShowSwipe extends View {
 					xStartRaw = e.getRawX();
 					xStart = e.getRawX() - deltaX;
 					timeStart = System.currentTimeMillis();
-					//Log.d("debug", "Touch down: " + startX);
+					
+					stateChanged(State.TOUCHED);
 					
 				} else if (e.getAction() == MotionEvent.ACTION_MOVE) {
 					
@@ -114,7 +205,7 @@ public class SlideShowSwipe extends View {
 				} else if (e.getAction() == MotionEvent.ACTION_UP){
 					
 					timeStart = System.currentTimeMillis();
-
+					
 					// correct deceleration coefficient sign
 					kV = v0 > 0 ? +Math.abs(kV) : -Math.abs(kV);
 					
@@ -135,7 +226,7 @@ public class SlideShowSwipe extends View {
 								v0 = +1;
 							} else {
 								xEnd = -deltaX;
-								v0 = -1;				
+								v0 = -1;	
 							}
 						} else if (deltaX < 0 && deltaX > -w / 2){
 							if (strong){
@@ -150,7 +241,7 @@ public class SlideShowSwipe extends View {
 							v0 = +1;		
 						} else if (deltaX < 0 && deltaX < -w / 2){						
 							xEnd = -w - deltaX;
-							v0 = -1;						
+							v0 = -1;			
 						} 
 						
 					} else {
@@ -165,6 +256,8 @@ public class SlideShowSwipe extends View {
 					v0 = Math.signum(v0) * (float) Math.sqrt(Math.abs(2.0 * kV * xEnd));
 			        vC = v0;
 					xCPrec = 0;
+					
+					stateChanged(State.RELEASED);
 					
 					self.invalidate();
 				}
@@ -181,11 +274,13 @@ public class SlideShowSwipe extends View {
 	@Override
 	protected void onSizeChanged (int w, int h, int oldw, int oldh){
 		super.onSizeChanged(w, h, oldw, oldh);
+		
 		// update dimensions
 		rectDimensions.left = 0;
 		rectDimensions.right = w;
 		rectDimensions.top = 0;
 		rectDimensions.bottom = h;
+		
 		// update decceleration coefficient
 		kV = kVScreen * w;
 	}
@@ -198,17 +293,19 @@ public class SlideShowSwipe extends View {
 	protected void onDraw(Canvas c){
 		super.onDraw(c);
 
-		if (bitmapFront == null){  // if nothing is drawn yet
+		// if nothing is drawn yet
+		if (!started){  
 			bitmapFront = container.getBitmapCurrent();
 			bitmapBack = bitmapFront;
-			if (bitmapFront != null){
-				 makeCalculations(c);
-			     c.drawBitmap(bitmapFront, null, rectDstF, null);	
-			}
+			makeCalculations(c);
+		    if (bitmapFront != null)
+		    	c.drawBitmap(bitmapFront, null, rectDstF, null);	
 		} else {
 			makeCalculations(c);
-			c.drawBitmap(bitmapFront, null, rectDstF, paintAlphaF);
-			c.drawBitmap(bitmapBack, null, rectDstB, paintAlphaB);
+			if (bitmapFront != null)
+				c.drawBitmap(bitmapFront, null, rectDstF, paintAlphaF);
+			if (bitmapBack != null)
+				c.drawBitmap(bitmapBack, null, rectDstB, paintAlphaB);
 			if (v0 != 0 && vC != 0)
 				self.invalidate();
 		}
@@ -223,6 +320,31 @@ public class SlideShowSwipe extends View {
 	 */
 	private void makeCalculations(Canvas c){
 		
+		// first call
+		if (!started){
+			if (bitmapFront != null)
+				rectDstFOrig = calculateRectDst(bitmapFront, rectDimensions);
+			rectDstBOrig = rectDstFOrig;
+			started = true;
+			bitmapChanged();
+		}
+		
+		// first call from stable state
+		if (!startedMove){
+			if (deltaX > 0){
+				bitmapBack = container.getBitmapPrevious();
+				if (bitmapBack != null)
+					rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+				startedMove = true;
+			} else if (deltaX < 0){
+				bitmapBack = container.getBitmapNext();
+				if (bitmapBack != null)
+					rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+				startedMove = true;
+			}
+		}
+		
+
 		// perform self motion
 		if (v0 != 0 && vC != 0)
 		{
@@ -231,9 +353,22 @@ public class SlideShowSwipe extends View {
 			xC = v0 * t - kV * t * t / 2f;
 			deltaX += xC - xCPrec;
 			xCPrec = xC;
-			if ((vC < 0 && v0 > 0) || (vC > 0 && v0 < 0)){ // velocity changed sign: stop self motion
+			
+			// velocity changed sign: stop self motion
+			if ((vC < 0 && v0 > 0) || (vC > 0 && v0 < 0)){ 
+	
+				// if image didn't reach opposite edge of view 
+				if (v0 < 0 && deltaX < -c.getWidth() / 2 && deltaX > -c.getWidth()){
+					deltaX = -c.getWidth();
+				} else if (v0 > 0 && deltaX > c.getWidth() / 2 && deltaX < c.getWidth()){
+					deltaX = c.getWidth();
+				} else {
+					deltaX = 0;
+					deltaXPrec = 0;
+				}
 				vC = 0;
 				v0 = 0;
+				stateChanged(State.STOPPED);
 			}
 		}
 		
@@ -244,57 +379,63 @@ public class SlideShowSwipe extends View {
 			deltaX -= c.getWidth();
 			bitmapFront = bitmapBack;
 			rectDstFOrig = rectDstBOrig; 
-		}
-		while (deltaX < -c.getWidth()){
+			undoAtZero = false;
+			bitmapChanged();
+		} 
+		while (deltaX <= -c.getWidth()){
 			xStart -= c.getWidth();
 			deltaXPrec += c.getWidth();
 			deltaX += c.getWidth();
 			bitmapFront = bitmapBack;
 			rectDstFOrig = rectDstBOrig;
+			undoAtZero = false;
+			bitmapChanged();
 		}
 		
-		// proceed various movement situations
-		if (!started){
-			rectDstFOrig = calculateRectDst(bitmapFront, rectDimensions);
+		// called when movement stops to cancel previous bitmap request 
+		if (startedMove && deltaX == 0 && deltaXPrec == 0 && undoAtZero){
+			container.undoGetBitmap();
+			bitmapBack = bitmapFront;
 			rectDstBOrig = rectDstFOrig;
-			started = true;
-		}
-		if (!startedMove){
-			if (deltaX > 0){
-				bitmapBack = container.getBitmapPrevious();
-				rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
-				startedMove = true;
-			} else if (deltaX < 0){
-				bitmapBack = container.getBitmapNext();
-				rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
-				startedMove = true;
-			}
+			bitmapChanged();
 		}
 		
-		if (deltaX >= 0 && deltaXPrec < 0){ // left side of back image crossed left border of view
+
+		// left side of back image crossed left border of view
+		if (deltaX > 0 && deltaXPrec <= 0){ 
         	if (bitmapFront != bitmapBack)
         		container.undoGetBitmap();
 			bitmapBack = container.getBitmapPrevious();
-			rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+			if (bitmapBack != null)
+				rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+			undoAtZero = true;
 			
-		} else if (deltaX < 0 && deltaXPrec >= 0){ // right side of back image crossed right border of view
+		// right side of back image crossed right border of view	
+		} else if (deltaX < 0 && deltaXPrec >= 0){ 
         	if (bitmapFront != bitmapBack)
         		container.undoGetBitmap();
 			bitmapBack = container.getBitmapNext();
-			rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+			if (bitmapBack != null)
+				rectDstBOrig = calculateRectDst(bitmapBack, rectDimensions);
+			undoAtZero = true;
 		}
-		
+
 		deltaXPrec = deltaX;
 		
-		rectDstF.left = rectDstFOrig.left + (int)deltaX;
-		rectDstF.top = rectDstFOrig.top;
-		rectDstF.right = rectDstFOrig.right + (int)deltaX;
-		rectDstF.bottom = rectDstFOrig.bottom; 
+		if (rectDstFOrig != null){
+			rectDstF.left = rectDstFOrig.left + (int)deltaX;
+			rectDstF.top = rectDstFOrig.top;
+			rectDstF.right = rectDstFOrig.right + (int)deltaX;
+			rectDstF.bottom = rectDstFOrig.bottom; 
+		}
 		
-		rectDstB.left = (int)deltaX - rectDstBOrig.right;
-		rectDstB.right = (int)deltaX - rectDstBOrig.left;
-		rectDstB.top = rectDstBOrig.top;
-		rectDstB.bottom = rectDstBOrig.bottom;
+		if (rectDstBOrig != null){
+			rectDstB.left = (int)deltaX - rectDstBOrig.right;
+			rectDstB.right = (int)deltaX - rectDstBOrig.left;
+			rectDstB.top = rectDstBOrig.top;
+			rectDstB.bottom = rectDstBOrig.bottom;
+		}
+			
 		if (deltaX < 0){
 			rectDstB.left += 2 * c.getWidth();
 			rectDstB.right += 2 * c.getWidth();
@@ -306,14 +447,39 @@ public class SlideShowSwipe extends View {
 	
 	
 	/**
+	 *	Set current state to changed and perform listener callback 
+	 */
+	private void stateChanged(State s){
+		if (s != stateCurrent){
+			stateCurrent = s;
+			if (stateChangeListener != null)
+				stateChangeListener.onStateChange(s);
+		}
+	}
+	
+	/**
+	 * Notify that current displaying bitmap was changed
+	 */
+	private void bitmapChanged(){
+		if (stateChangeListener != null)
+			stateChangeListener.onCurrentBitmapChange();
+	}
+	
+	/**
 	 * Returns rectangle that contains position of a given bitmap in coordinates of destination 
 	 * rectangle, such that the bitmap fits it aligned to center and scaled proportionally
 	 * 
 	 * @param b		source bitmap
 	 * @param d		destination rectangle
 	 * @return		position in coordinates of destination rectangle
+	 * @throws	NullPointerException
 	 */
-	static private Rect calculateRectDst(Bitmap b, Rect d){
+	static private Rect calculateRectDst(Bitmap b, Rect d) throws NullPointerException {
+		if (b == null) 
+			throw new NullPointerException("Bitmap is null");
+		if (d == null)
+			throw new NullPointerException("Destination Rect object is null");
+		
 		Rect dst = new Rect();
 		if ((float)b.getWidth()/(float)b.getHeight() < (float)d.width()/(float)d.height()){
 			dst.left = (int)((float)d.width() / 2f - (float)b.getWidth() * (float)d.height() / (float)b.getHeight() / 2f);
--- src/ru/salauyou/slideshowswipedemo/ActivityMain.java
+++ src/ru/salauyou/slideshowswipedemo/ActivityMain.java
@@ -2,19 +2,23 @@ package ru.salauyou.slideshowswipedemo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import ru.salauyou.slideshowswipe.SlideShowSwipe;
+import ru.salauyou.slideshowswipe.SlideShowSwipe.State;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
-public class ActivityMain extends Activity implements SlideShowSwipe.BitmapContainer {
+public class ActivityMain extends Activity implements SlideShowSwipe.BitmapContainer, SlideShowSwipe.OnStateChangeListener {
 
 	SlideShowSwipe slideShow;
+	ImageView viewControl;
 	List<Bitmap> container;
+	Random rnd = new Random();
 	
 	int posCurrent = 0;
 	int posPrec = 0;
@@ -27,11 +31,18 @@ public class ActivityMain extends Activity implements SlideShowSwipe.BitmapConta
 		slideShow = (SlideShowSwipe)findViewById(R.id.slide_show);
 		slideShow.setBitmapContainer(this);
 		
+		viewControl = (ImageView)findViewById(R.id.view_control);
+		slideShow.setOnStateChangeListener(this);
+		
 		container = new ArrayList<Bitmap>();
 		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img01));
 		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img02));
 		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img03));
 		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img04));
+		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img05));
+		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img06));
+		container.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.img07));
+        
 		
 	}
 
@@ -49,35 +60,57 @@ public class ActivityMain extends Activity implements SlideShowSwipe.BitmapConta
 	@Override
 	public Bitmap getBitmapNext() {
 		posPrec = posCurrent;
+		
+		/*
 		posCurrent ++;
 		if (posCurrent >= container.size()){
 			posCurrent = 0;
-		}
-		//Log.d("debug", "Got next: " + posCurrent);
+		}*/
+		
+		while (posCurrent == posPrec)  // random to simulate mutation
+			posCurrent = rnd.nextInt(container.size());
+		
 		return container.get(posCurrent);
 	}
 
 	@Override
 	public Bitmap getBitmapPrevious() {
 		posPrec = posCurrent;
+	
+		/*
 		posCurrent --;
 		if (posCurrent < 0){
 			posCurrent = container.size() - 1;
-		}
-		//Log.d("debug", "Got previous: " + posCurrent);
+		}*/
+		
+		while (posCurrent == posPrec)   // random to simulate mutation
+			posCurrent = rnd.nextInt(container.size());  
+		
 		return container.get(posCurrent);
 	}
 
 	@Override
 	public Bitmap getBitmapCurrent() {
-		//Log.d("debug", "Got current: " + posCurrent);
 		return container.get(posCurrent);
 	}
 
 	@Override
 	public void undoGetBitmap() {
 		posCurrent = posPrec;
-		//Log.d("debug", "Undid: " + posCurrent);
+	}
+
+	
+	//---------------------------------------
+	
+	
+	@Override
+	public void onStateChange(State state) {
+		//viewControl.setImageBitmap(this.getBitmapCurrent());
+	}
+
+	@Override
+	public void onCurrentBitmapChange() {
+		viewControl.setImageBitmap(this.getBitmapCurrent());
 	}
 
 }
